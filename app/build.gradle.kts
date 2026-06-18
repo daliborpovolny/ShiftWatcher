3e09c2f87a2c3ad813d948026422a01fc7e1a8f9
Dalibor Povolny <dalibor.povolny@gmail.com>
2026-06-18 19:51:51 +0200

    bump up version number and compile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.daliborpovolny.shiftwatcher"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
