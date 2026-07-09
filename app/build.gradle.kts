9fe3c415ed2ab81f84c1aa41e80e3fb37cc09abc
Dalibor Povolny <dalibor.povolny@gmail.com>
2026-07-09 22:29:36 +0200

    bump up app version code to test if update process works

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.daliborpovolny.shiftwatcher"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
+        versionCode = 6
         versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
